ce2d6e1dca6424e55f813bd2546c65c9a78b0235
justinsasek <justinsasek@gmail.com>
2024-01-25 15:49:47 -0600

    generalized VQVAE with config dict

--- VQVAE.py
+++ VQVAE.py
@@ -2,112 +2,130 @@ import torch
 from torch import nn, optim
 from torch.nn import functional as F
 from torchvision.transforms import CenterCrop as crop
-from typing import Tuple
+from typing import Tuple, List
 
-SIZES = ((28, 28, 1), (16, 16, 4), (8, 8, 8), (4, 4, 16))
-NUM_LAYERS_PER_CHANNEL_SIZE = 4
 
-# class Conv_Block(nn.module):
-#     def __init__(self, in_ch:int, out_ch:int, n_layers:int):
-#         super().__init__()
-#         self.mods = []
-#         for i in range(n_layers):
-            
-#             self.mods.append(nn.Sequential(
-#                 nn.Conv2d(in_ch, out_ch, 5, stride=1, padding="same"),
-#                 nn.BatchNorm2d(out_ch),
-#                 nn.Tanh(),
-#             ))
-#             in_ch = out_ch
+class ConvBlock(nn.Module):
+    def __init__(self, in_ch: int, out_ch: int, n_layers: int):
+        super().__init__()
+        mods = []
+        for i in range(n_layers):
+            layer_in_ch = in_ch if i == 0 else max(in_ch, out_ch)
+            layer_out_ch = out_ch if i == n_layers - 1 else max(in_ch, out_ch)
+            mods.append(
+                nn.Sequential(
+                    nn.Conv2d(layer_in_ch, layer_out_ch, 5, stride=1, padding="same"),
+                    nn.BatchNorm2d(layer_out_ch),
+                    nn.Tanh(),
+                )
+            )
+
+        self.mods = nn.Sequential(*mods)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.mods(x)
+        return x
 
 
 class Encoder(nn.Module):
-    def __init__(self):
+    def __init__(self, config: dict):
         super().__init__()
-        sizes = SIZES
-        num_layers_per_channel_size = NUM_LAYERS_PER_CHANNEL_SIZE
-        
-        nn_modules = []
-        
-        for i in range(len(sizes)):
-            for j in range(num_layers_per_channel_size):
-                start_n_channels = sizes[i][2]
-                end_n_channels = sizes[i][2] if i == len(sizes) - 1 else sizes[i + 1][2]
-                
-                if j != 0:
-                    start_n_channels = end_n_channels
-                    
-                nn_modules.append(nn.Sequential(
-                    nn.Conv2d(start_n_channels, end_n_channels, 5, stride=1, padding="same"),
-                    nn.BatchNorm2d(end_n_channels),
-                    nn.Tanh(),
-                ))
-            if i != len(sizes) - 1:
-                nn_modules.append(nn.MaxPool2d(2, 2))
-            
-        self.nn_modules = nn.Sequential(*nn_modules)
+
+        self.config = config
+        mods: List[nn.Module] = []
+
+        for i in range(len(config["n_channels"]) - 1):
+            mods.append(
+                ConvBlock(
+                    config["n_channels"][i],
+                    config["n_channels"][i + 1],
+                    config["num_layers_per_channel_size"],
+                )
+            )
+            mods.append(nn.MaxPool2d(2, 2))
+
+        mods.append(
+            ConvBlock(
+                config["n_channels"][-1],
+                config["n_channels"][-1],
+                config["num_layers_per_channel_size"],
+            )
+        )
+
+        self.mods = nn.Sequential(*mods)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = F.pad(x, pad=(2, 2, 2, 2), mode="constant", value=0)
-        x = self.nn_modules(x)
+        ideal_size = max(self.config["input_size"])
+        ideal_size = 2 ** (ideal_size - 1).bit_length()
+        left_pad = (ideal_size - self.config["input_size"][1]) // 2
+        right_pad = ideal_size - self.config["input_size"][1] - left_pad
+        top_pad = (ideal_size - self.config["input_size"][0]) // 2
+        bottom_pad = ideal_size - self.config["input_size"][0] - top_pad
+
+        x = F.pad(
+            x, pad=(left_pad, right_pad, top_pad, bottom_pad), mode="constant", value=0
+        )
+        x = self.mods(x)
         return x
 
 
 class Decoder(nn.Module):
-    def __init__(self):
+    def __init__(self, config: dict):
         super().__init__()
-        sizes = SIZES[::-1]
-        num_layers_per_channel_size = NUM_LAYERS_PER_CHANNEL_SIZE
-        
-        nn_modules = []
-        
-        for i in range(len(sizes) - 1):
-            for j in range(num_layers_per_channel_size):
-                start_n_channels = sizes[i][2]
-                end_n_channels = sizes[i][2] if i == len(sizes) - 1 else sizes[i + 1][2]
-                
-                if j != len(sizes) - 1:
-                    end_n_channels = start_n_channels
-                
-                nn_modules.append(nn.Sequential(
-                    nn.Conv2d(start_n_channels, end_n_channels, 5, stride=1, padding="same"),
-                    nn.BatchNorm2d(end_n_channels),
-                    nn.Tanh(),
-                ))
-            if i != len(sizes) - 1:
-                nn_modules.append(nn.Upsample(scale_factor=2))
-        
-        self.nn_modules = nn.Sequential(*nn_modules)
-        
-        self.crop = crop(size=(sizes[-1][0], sizes[-1][1]))
+
+        mods: List[nn.Module] = []
+
+        n_channels_r = config["n_channels"][::-1]
+
+        for i in range(len(n_channels_r) - 1):
+            mods.append(
+                ConvBlock(
+                    n_channels_r[i],
+                    n_channels_r[i + 1],
+                    config["num_layers_per_channel_size"],
+                )
+            )
+            mods.append(nn.Upsample(scale_factor=2))
+
+        mods.append(
+            ConvBlock(
+                n_channels_r[-1],
+                n_channels_r[-1],
+                config["num_layers_per_channel_size"],
+            )
+        )
+
+        self.mods = nn.Sequential(*mods)
+
+        self.crop = crop(size=config["input_size"])
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.nn_modules(x)
+        x = self.mods(x)
         x = self.crop(x)
         return x
 
 
 class VQVAE(nn.Module):
-    def __init__(self):
+    def __init__(self, config: dict):
         super().__init__()
-        self.sizes = SIZES
 
-        self.code_book_size = 5
+        self.config = config
+        self.latent_size = config["n_channels"][-1]
 
-        self.encoder = Encoder()
-        self.decoder = Decoder()
-        self.codebook = nn.Embedding(self.code_book_size, self.sizes[-1][2])
+        self.encoder = Encoder(config)
+        self.decoder = Decoder(config)
+        self.codebook = nn.Embedding(config["codebook_size"], self.latent_size)
 
     def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         x = self.encoder(x)
 
         # lookup
-        x_flat = x.view(size=(-1, self.sizes[-1][2]))
+        x_flat = x.view(size=(-1, self.latent_size))
         dists = torch.cdist(x_flat, self.codebook.weight, 2)
         x_vq = self.codebook(torch.argmin(dists, dim=1))
-        
-        x_vq = x_vq.view(size=(-1, self.sizes[-1][2], self.sizes[-1][0], self.sizes[-1][1]))
-        
+
+        x_vq = x_vq.view(size=(-1, self.latent_size, *self.config["hidden_size"]))
+
         x_vq = x + (x_vq - x).detach()
 
         # find loss
--- main.py
+++ main.py
@@ -7,32 +7,36 @@ import numpy as np
 import os
 from VQVAE import VQVAE
 from torchvision.datasets import MNIST
+
 matplotlib.use("Qt5Agg")
 
 CKPT_PATH = "vqvae.ckpt"
 
 data = MNIST(root="Data", train=True, download=True).data
 
-vqvae = VQVAE()
+config = {
+    "input_size": (28, 28),
+    "hidden_size": (4, 4),
+    "n_channels": [1, 4, 8, 16],
+    "num_layers_per_channel_size": 4,
+    "codebook_size": 5,
+    "commitment_loss_scaler": 0.25,
+    "vqvae_loss_scaler": 0.25,
+}
+
+vqvae = VQVAE(config)
 optimizer = optim.Adam(vqvae.parameters(), lr=1e-3)
 
 # load model
 if os.path.isfile(CKPT_PATH):
     vqvae.load_state_dict(torch.load(CKPT_PATH))
 
-# if torch.cuda.is_available():
-#     print("Using Cuda")
-#     vqvae = vqvae.cuda()
-
-# elif torch.backends.mps.is_available():
-#     print("Using MPS")
-#     mps_device = torch.device("mps")
-#     vqvae = vqvae.to(mps_device)
-#     data = data.to(mps_device)
+if torch.cuda.is_available():
+    print("Using Cuda")
+    vqvae = vqvae.cuda()
 
 mean = torch.mean(data.float())
 std = torch.std(data.float())
-
 data_norm = data.float() - mean
 data_norm = data_norm / std
 data_norm = data_norm[:100, :, :].unsqueeze(1)
@@ -45,20 +49,18 @@ for ax in axes.flatten():
 
 for ax, img in zip(axes[:, :10].flatten(), data[:100]):
     ax.imshow(img, cmap="gray")
-    
-plt.pause(0.01)
 
+plt.pause(0.01)
 
 for i in range(100000):
     print(i, end="\r")
     output, vqvae_loss = vqvae(data_norm)
-    beta_2 = 0.25
-    loss = F.mse_loss(output, data_norm) + beta_2 * vqvae_loss
+    loss = F.mse_loss(output, data_norm) + config["vqvae_loss_scaler"] * vqvae_loss
     loss.backward()
     optimizer.step()
     optimizer.zero_grad()
 
-    if i % 10 == 0:
+    if i % 100 == 0:
         for ax, img in zip(axes[:, 11:].flatten(), output[:100]):
             img = img * std + mean
             img = img.detach().cpu().squeeze().numpy()
@@ -66,8 +68,6 @@ for i in range(100000):
             ax.imshow(img, cmap="gray")
 
         plt.pause(0.01)
-        
+
         # save model
         torch.save(vqvae.state_dict(), CKPT_PATH)
-        
-    
